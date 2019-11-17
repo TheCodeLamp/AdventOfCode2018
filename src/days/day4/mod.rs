e6051c7db004de0c51e85917ed9af3f46308cb80
Simon P <simon@spectral.ninja>
2019-11-17 20:19:50 +0100

    almost 4

--- src/days/day4/mod.rs
+++ src/days/day4/mod.rs
@@ -1,20 +1,85 @@
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{BufReader, BufRead, BufWriter, Write};
 
 use regex::Regex;
 use std::fmt::{Formatter, Error};
+use std::collections::{HashMap, HashSet};
 
-
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct Guard {
+    id: usize,
+    sleep_total: usize,
+    sleep_max: usize,
+    sleep_start: Option<usize>,
+}
 
 pub fn run_part_1() {
     let mut input = read(match File::open("src/days/day4/input") {
         Ok(x) => x,
-        _ => panic!(),
+        _ => panic!("File for day 4 could'nt read."),
     });
 
-    input.sort_unstable_by(|a,b | a.0.cmp(&b.0));
 
+    input.sort_unstable_by(|a, b| a.0.cmp(&b.0));
 
+    let mut guard_map = HashMap::<usize, Guard>::new();
+    let mut guards = HashSet::<usize>::new();
+    let mut current_guard= 0;
+    let mut highest_guard = (0,0);
+
+    for (date_time, state) in input {
+        match state {
+            SleepState::BeginsShift(id) => {
+                if guard_map.contains_key(&id) {
+                    current_guard = id;
+                    assert_eq!(guard_map.get(&id).unwrap().sleep_start, None);
+                } else {
+                    current_guard = id;
+                    guards.insert(id);
+                    guard_map.insert(id, Guard {
+                        id,
+                        sleep_total: 0,
+                        sleep_max: 0,
+                        sleep_start: None,
+                    });
+                }
+            }
+
+            SleepState::FallsAsleep => {
+                let temp_guard = guard_map.get_mut(&current_guard).unwrap();
+                assert_eq!(temp_guard.sleep_start, None);
+                temp_guard.sleep_start = Some(date_time);
+            }
+
+            SleepState::WakesUp => {
+                let temp_guard = guard_map.get_mut(&current_guard).unwrap();
+                assert_ne!(temp_guard.sleep_start, None);
+                let time = date_time - temp_guard.sleep_start.unwrap();
+                if temp_guard.sleep_max < time{
+                    temp_guard.sleep_max = time;
+                }
+                temp_guard.sleep_total += time;
+                temp_guard.sleep_start = None;
+                if temp_guard.sleep_total > highest_guard.1{
+                    highest_guard.0 = temp_guard.id;
+                    highest_guard.1 = temp_guard.sleep_total;
+                }
+            }
+        }
+    }
+
+    let mut writer = BufWriter::new(File::create("src/days/day4/output_part1.txt").unwrap());
+
+    let mut guard_list: Vec<usize> = guards.clone().into_iter().collect();
+    guard_list.sort_unstable();
+
+    for guard in guard_list {
+        let current = guard_map.get(&guard).unwrap();
+        writer.write(format!("Guard {} slept {} time.", current.id, current.sleep_total).as_bytes()).unwrap();
+        writer.write("\n".as_bytes()).unwrap();
+    }
+
+    println!("Awnser is {} * {} = {}", highest_guard.0, guard_map.get(&highest_guard.0).unwrap().sleep_max, highest_guard.0*guard_map.get(&highest_guard.0).unwrap().sleep_max)
 
 }
 
@@ -27,14 +92,10 @@ pub fn run_part_2() {
 }
 
 
-
 fn read(f: File) -> Vec<(usize, SleepState)> {
     let lines = BufReader::new(f).lines();
 
-    let mut output = Vec::<(usize, SleepState)>::with_capacity(match lines.size_hint() {
-        (x, Some(_)) => x,
-        _ => 0,
-    });
+    let mut output = Vec::<(usize, SleepState)>::with_capacity(lines.size_hint().0);
 
     for line in lines {
         if let Ok(line_ok) = line {
@@ -67,7 +128,6 @@ fn parse_to_sleep_state(input: &str) -> Result<SleepState, &str> {
         _ => Result::Ok(SleepState::BeginsShift(
             guard_regex.captures(&state_string).unwrap()[1].parse().unwrap())),
     };
-
 }
 
 fn parse_to_int(input: &str) -> Result<usize, &str> {
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,4 +1,7 @@
 use std::time::Instant;
+use std::fs::File;
+use std::path::Path;
+use std::io::{BufReader, BufRead, BufWriter, Write};
 
 mod day1;
 mod day2;
@@ -45,4 +48,27 @@ pub fn run_day_1() {
     day1::run_part_2();
 
     println!("{} ms", time.elapsed().as_millis())
+}
+
+#[allow(dead_code)]
+pub fn tests(){
+    let input_file = File::open(Path::new("src/days/day4/input")).expect("Can't find file for day 4 in tests.");
+    let output_file = File::create(Path::new("out/sorted_day4.txt")).unwrap();
+    let reader = BufReader::new(input_file).lines();
+
+    let mut out_vec = Vec::<String>::with_capacity(reader.size_hint().0);
+
+    for line in reader{
+        out_vec.push(line.unwrap());
+    }
+
+    out_vec.sort_unstable();
+
+    let mut writer = BufWriter::with_capacity(out_vec.len(), output_file);
+
+    for line in out_vec{
+        writer.write(line.as_bytes()).unwrap();
+        writer.write("\n".as_bytes()).unwrap();
+    }
+
 }
